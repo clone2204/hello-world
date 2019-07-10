63b59f09618ca385fe3aa396f750366a3cfa481a
Will <wpjack1@gmail.com>
2019-07-10 11:56:43 -0400

    Testing Travis

---
+++ Test.java
@@ -0,0 +1,23 @@
+
+public class Test
+{
+   public static void main(String[] args)
+   {
+      int a = 5;
+      int b = 12;
+
+      System.out.println("Biggest Brain Add: " + a + " + " + b + " = " + BigBrainAdd(a, b));
+   }
+
+   public int BigBrainAdd(int a, int b)
+   {
+      int result = a;
+      
+      for(int loop = 0; loop < b; loop++)
+      {
+         result++;
+      }
+
+      return result;
+   }
+}
